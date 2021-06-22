349ee91b4432df6080656f474d92c20257cc800c
Amirhossein Meydani <amirhwsin@outlook.com>
2021-06-22 13:32:11 +0430

    Some bugs fixed

--- scripts/gh-pages-deploy.js
+++ scripts/gh-pages-deploy.js
@@ -6,15 +6,14 @@ const fs = require("fs");
     await execa("git", ["checkout", "--orphan", "gh-pages"]);
     // eslint-disable-next-line no-console
     console.log("Building started...");
-    await execa("npm", ["run", "build"]);
-    // await execa("yarn", ["build"]);
+    // await execa("npm", ["run", "build"]);
+    await execa("yarn", ["build"]);
     // Understand if it's dist or build folder
-    const folderName = fs.existsSync("dist") ? "dist" : "build";
-    await execa("git", [folderName, "add", "--all"]);
-    await execa("git", [folderName, "commit", "-m", "gh-pages"]);
+    await execa("git", ["add", "--all"]);
+    await execa("git", ["commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await execa("rm", ["-r", folderName]);
+    // await execa("rm", ["-r", folderName]);
     await execa("git", ["checkout", "-f", "develop"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
