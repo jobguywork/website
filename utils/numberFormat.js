23818f66985e58e7fd3615f2616203421751f2a0
Ali <a.pamenari@alibaba.ir>
2021-07-14 09:47:46 +0430

    feat: add lint-staged

--- utils/numberFormat.js
+++ utils/numberFormat.js
@@ -1,9 +1,9 @@
 export const numberFormat = (value) => {
-    if (isNaN(value)) return value
-    return value
-        .toLocaleString('ir', {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 2,
-        })
-        .split('.')[0]
+  if (isNaN(value)) return value
+  return value
+    .toLocaleString('ir', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })
+    .split('.')[0]
 }
