7ca275c5314ac6e80079295e98b3bd7700529b09
Ali Pamenari <pamenary@gmail.com>
2021-08-07 20:08:44 +0430

    chore: change comment

--- nuxt.config.js
+++ nuxt.config.js
@@ -201,7 +201,7 @@ module.exports = {
           cacheName: 'api',
           cacheExpiration: {
             maxEntries: 300,
-            maxAgeSeconds: 300, // 15 minutes
+            maxAgeSeconds: 300, // 5 minutes
           },
           cacheableResponse: { statuses: [200] },
         },
